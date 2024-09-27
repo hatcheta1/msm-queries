730ad515133198cb41b749fb3fa83530ab6be382
Ashanti Hatchett <ahatchett02@gmail.com>
2024-09-27 18:03:11 +0000

    finished directors page

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,12 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "director_templates/list" })
   end
+
+  def show
+    @the_id = params.fetch("the_id")
+
+    @director = Director.where({ :id => @the_id }).first
+
+    render({ :template => "director_templates/details" })
+  end
 end
